d25d4da0abac12d7b5420061ad5c080cf643b259
Claas <claas@claas.dev>
2026-01-11 02:37:54 +0100

    Make it variable

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,49 @@ import { createEffect, createSignal } from "solid-js";
 import "./App.css";
 
 function App() {
-  const [depth, setDepth] = createSignal(20);
+  const [y1, setY1] = createSignal(10);
 
   createEffect(() =>
-    document.documentElement.style.setProperty("--depth", depth() + "%")
+    document.documentElement.style.setProperty("--y1", y1() + "%")
+  );
+
+  const [y2, setY2] = createSignal(20);
+
+  createEffect(() =>
+    document.documentElement.style.setProperty("--y2", y2() + "%")
+  );
+
+  const [slope, setSlope] = createSignal(0);
+
+  createEffect(() =>
+    document.documentElement.style.setProperty("--slope", slope() + "%")
   );
 
   return (
     <>
-      <label for="depth">Depth</label>
+      <label for="y1">Y1</label>
       <input
-        id="depth"
+        id="y1"
         type="range"
         max="100"
         value="20"
-        onInput={(event) => setDepth(+event.currentTarget.value)}
+        onInput={(event) => setY1(+event.currentTarget.value)}
+      />
+      <label for="y2">Y2</label>
+      <input
+        id="y2"
+        type="range"
+        max="100"
+        value="20"
+        onInput={(event) => setY2(+event.currentTarget.value)}
+      />
+      <label for="slope">Slope</label>
+      <input
+        id="slope"
+        type="range"
+        max="100"
+        value="0"
+        onInput={(event) => setSlope(+event.currentTarget.value)}
       />
     </>
   );
